09fcfa31d08cec636cf04f2afbd0979d54008f62
Mehmet Deniz Gucdemir <md.gucdemir@gmail.com>
2023-12-05 03:20:01 +0300

    daily working

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from "react";
 
-import NoPoster from '../../assets/no-poster.png'
-import MovieControls from '../movieControls/MovieControls';
+import NoPoster from "../../assets/no-poster.png";
+import MovieControls from "../movieControls/MovieControls";
 
+const MovieCard = ({ movie, type }) => {
+  const posterUrl = "https://image.tmdb.org/t/p/w200/" + movie.poster_path;
 
-const MovieCard = ({movie, type}) => {
-  const posterUrl = movie.poster_path ? "https://image.tmdb.org/t/p/w200/" + movie.poster_path : NoPoster;
-    
   return (
-    <div className='movie-card'>
-        <div className="overlay"></div>
-        {movie.poster_path ? (
-                <img src={posterUrl} alt={`${movie.poster_path}`} />
-            ) : (
-                <div className='filler-poster'></div>
-            )}
-            <MovieControls movie={movie} type={type} />
-
+    <div className="movie-card">
+      <div className="overlay"></div>
+      {movie.poster_path ? (
+        <img src={posterUrl} alt={`${movie.poster_path}`} />
+      ) : (
+        <div className="filler-poster">
+          <img src={NoPoster} alt="" />
+        </div>
+      )}
+      <MovieControls movie={movie} type={type} />
     </div>
-  )
-}
+  );
+};
 
-export default MovieCard
+export default MovieCard;
